7d788ab757e868fd836194d37e151267f7fb6bb8
Ronald M. Kasendwa <kasendwaronald@gmail.com>
2026-09-04 18:46:13 +0300

    feat: add a first-game tutorial (#53)

--- apps/web/features/game/index.ts
+++ apps/web/features/game/index.ts
@@ -1,3 +1,7 @@
 export { Board, type BoardProps } from "./components/Board";
 export { PlayGame, type PlayGameProps } from "./components/PlayGame";
-export { DIFFICULTY_STORAGE_KEY, GameSetup } from "./components/GameSetup";
+export {
+  DIFFICULTY_STORAGE_KEY,
+  TUTORIAL_STORAGE_KEY,
+  GameSetup,
+} from "./components/GameSetup";
